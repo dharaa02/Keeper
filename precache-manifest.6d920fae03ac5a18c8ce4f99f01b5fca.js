360ea694d250f7fbf58903efb656013cb8d57e29
Dharaa <dharaasharma2001@gmail.com>
2022-11-18 00:46:56 +0530

    Updates

---
+++ precache-manifest.6d920fae03ac5a18c8ce4f99f01b5fca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b599e88f45cbdf27c128a89975ed4c1",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "790083906c9debf35895",
+    "url": "/Keeper/static/js/2.12bfa857.chunk.js"
+  },
+  {
+    "revision": "986e2e7c1b96ac3a3534",
+    "url": "/Keeper/static/js/main.3c352ad4.chunk.js"
+  },
+  {
+    "revision": "4b772ed70d71f9d0beff",
+    "url": "/Keeper/static/js/runtime-main.fd6e374a.js"
+  }
+]);
